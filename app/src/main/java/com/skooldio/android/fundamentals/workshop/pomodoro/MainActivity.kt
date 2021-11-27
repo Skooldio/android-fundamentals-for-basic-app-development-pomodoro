7368bfd35c07db3d7930c9f47d7652460385db80
Akexorcist <akexorcist@gmail.com>
2021-11-27 21:16:22 +0700

    Add default value for min and max duration

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
@@ -5,9 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private const val DEFAULT_WORK_DURATION = 25
-        private const val DEFAULT_SHORT_BREAK_DURATION = 5
-        private const val DEFAULT_LONG_BREAK_DURATION = 25
+        private const val WORK_DURATION_DEFAULT = 25
+        private const val WORK_DURATION_MIN = 20
+        private const val WORK_DURATION_MAX = 40
+
+        private const val SHORT_BREAK_DURATION_DEFAULT = 5
+        private const val SHORT_BREAK_DURATION_MIN = 5
+        private const val SHORT_BREAK_DURATION_MAX = 15
+
+        private const val LONG_BREAK_DURATION_DEFAULT = 25
+        private const val LONG_BREAK_DURATION_MIN = 20
+        private const val LONG_BREAK_DURATION_MAX = 30
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
