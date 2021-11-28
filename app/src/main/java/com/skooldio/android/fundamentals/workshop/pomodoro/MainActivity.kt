ab03ac2a32a99a0b2aad5471924c74989d93cc58
Akexorcist <akexorcist@gmail.com>
2021-11-28 17:34:57 +0700

    Move constant value out from activity
    # Conflicts:
    #	app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
@@ -2,12 +2,21 @@ package com.skooldio.android.fundamentals.workshop.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skooldio.android.fundamentals.workshop.pomodoro.data.PomodoroConfig
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private const val DEFAULT_WORK_DURATION = 25
-        private const val DEFAULT_SHORT_BREAK_DURATION = 5
-        private const val DEFAULT_LONG_BREAK_DURATION = 25
+        private const val WORK_DURATION_DEFAULT = PomodoroConfig.WORK_DURATION_DEFAULT
+        private const val WORK_DURATION_MIN = PomodoroConfig.WORK_DURATION_MIN
+        private const val WORK_DURATION_MAX = PomodoroConfig.WORK_DURATION_MAX
+
+        private const val SHORT_BREAK_DURATION_DEFAULT = PomodoroConfig.SHORT_BREAK_DURATION_DEFAULT
+        private const val SHORT_BREAK_DURATION_MIN = PomodoroConfig.SHORT_BREAK_DURATION_MIN
+        private const val SHORT_BREAK_DURATION_MAX = PomodoroConfig.SHORT_BREAK_DURATION_MAX
+
+        private const val LONG_BREAK_DURATION_DEFAULT = PomodoroConfig.LONG_BREAK_DURATION_DEFAULT
+        private const val LONG_BREAK_DURATION_MIN = PomodoroConfig.LONG_BREAK_DURATION_MIN
+        private const val LONG_BREAK_DURATION_MAX = PomodoroConfig.LONG_BREAK_DURATION_MAX
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
