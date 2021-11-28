b4fa8b2a2a569273c9ae4446ef843f60dbbae9f0
Akexorcist <akexorcist@gmail.com>
2021-11-29 02:20:41 +0700

    Remove unused import

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/TimerActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/TimerActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.skooldio.android.fundamentals.workshop.pomodoro.data.PomodoroCounter
 import com.skooldio.android.fundamentals.workshop.pomodoro.databinding.ActivityTimerBinding
 
 class TimerActivity : AppCompatActivity() {
