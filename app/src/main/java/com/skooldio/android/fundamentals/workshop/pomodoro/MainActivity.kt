2c81a6eacd6154fcd397f18babfbdff08398ea5e
Akexorcist <akexorcist@gmail.com>
2021-11-28 17:34:57 +0700

    Move constant value out from activity

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
@@ -2,21 +2,22 @@ package com.skooldio.android.fundamentals.workshop.pomodoro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skooldio.android.fundamentals.workshop.pomodoro.data.PomodoroConfig
 import com.skooldio.android.fundamentals.workshop.pomodoro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private const val WORK_DURATION_DEFAULT = 25
-        private const val WORK_DURATION_MIN = 20
-        private const val WORK_DURATION_MAX = 40
+        private const val WORK_DURATION_DEFAULT = PomodoroConfig.WORK_DURATION_DEFAULT
+        private const val WORK_DURATION_MIN = PomodoroConfig.WORK_DURATION_MIN
+        private const val WORK_DURATION_MAX = PomodoroConfig.WORK_DURATION_MAX
 
-        private const val SHORT_BREAK_DURATION_DEFAULT = 5
-        private const val SHORT_BREAK_DURATION_MIN = 5
-        private const val SHORT_BREAK_DURATION_MAX = 15
+        private const val SHORT_BREAK_DURATION_DEFAULT = PomodoroConfig.SHORT_BREAK_DURATION_DEFAULT
+        private const val SHORT_BREAK_DURATION_MIN = PomodoroConfig.SHORT_BREAK_DURATION_MIN
+        private const val SHORT_BREAK_DURATION_MAX = PomodoroConfig.SHORT_BREAK_DURATION_MAX
 
-        private const val LONG_BREAK_DURATION_DEFAULT = 25
-        private const val LONG_BREAK_DURATION_MIN = 20
-        private const val LONG_BREAK_DURATION_MAX = 30
+        private const val LONG_BREAK_DURATION_DEFAULT = PomodoroConfig.LONG_BREAK_DURATION_DEFAULT
+        private const val LONG_BREAK_DURATION_MIN = PomodoroConfig.LONG_BREAK_DURATION_MIN
+        private const val LONG_BREAK_DURATION_MAX = PomodoroConfig.LONG_BREAK_DURATION_MAX
     }
 
     private val binding: ActivityMainBinding by lazy {
