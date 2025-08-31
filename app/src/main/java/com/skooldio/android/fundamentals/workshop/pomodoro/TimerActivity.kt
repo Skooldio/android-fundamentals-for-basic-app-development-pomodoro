41580f23c3a68014bae9ed83d78d0909b73b17cc
Akexorcist <akexorcist@gmail.com>
2025-09-01 03:58:46 +0700

    Workshop Part 9 - Bind all view to Pomodoro counter

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/TimerActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/TimerActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.view.View
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.skooldio.android.fundamentals.workshop.pomodoro.config.NotificationConfig
+import com.skooldio.android.fundamentals.workshop.pomodoro.data.PomodoroCounter
 import com.skooldio.android.fundamentals.workshop.pomodoro.databinding.ActivityTimerBinding
 
 class TimerActivity : AppCompatActivity() {
@@ -32,6 +34,8 @@ class TimerActivity : AppCompatActivity() {
 
     private var config: Config? = null
 
+    private val pomodoroCounter = PomodoroCounter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Additional code to support edge-to-edge in Android 15
@@ -48,10 +52,89 @@ class TimerActivity : AppCompatActivity() {
         }
 
         restoreBundle()
+        setupView()
+        setupPomodoroCounter()
     }
 
     private fun restoreBundle() {
         config = intent.getParcelableExtra(EXTRA_CONFIG)
+    }
+
+    private fun setupView() {
+        binding.buttonPlay.setOnClickListener {
+            binding.buttonPlay.visibility = View.GONE
+            binding.buttonPause.visibility = View.VISIBLE
+            pomodoroCounter.play()
+        }
+        binding.buttonPause.setOnClickListener {
+            binding.buttonPlay.visibility = View.VISIBLE
+            binding.buttonPause.visibility = View.GONE
+            pomodoroCounter.pause()
+        }
+        binding.buttonFastForward.setOnClickListener {
+            binding.buttonPlay.visibility = View.VISIBLE
+            binding.buttonPause.visibility = View.GONE
+            pomodoroCounter.fastForward()
+        }
+    }
+
+    private fun setupPomodoroCounter() {
+        config?.let {
+            pomodoroCounter.apply {
+                config(
+                    workDuration = it.workDuration,
+                    shortBreakDuration = it.shortBreakDuration,
+                    longBreakDuration = it.longBreakDuration
+                )
+                setListener(
+                    onReady = { minute, second -> updateCounterStatus(State.Ready, minute, second) },
+                    onWork = { minute, second -> updateCounterStatus(State.Work, minute, second) },
+                    onShortBreak = { minute, second -> updateCounterStatus(State.ShortBreak, minute, second) },
+                    onLongBreak = { minute, second -> updateCounterStatus(State.LongBreak, minute, second) }
+                )
+            }
+        }
+    }
+
+    private fun updateCounterStatus(state: State, minute: Int, second: Int) {
+        binding.textViewState.setText(getStateTextResourceId(state))
+        binding.textViewMinute.text = getString(R.string.time_value, minute)
+        binding.textViewSecond.text = getString(R.string.time_value, second)
+        if (minute == 0 && second == 0) {
+            onPomodoroCounterFinished(state)
+        }
+    }
+
+    private fun getStateTextResourceId(state: State): Int {
+        return when (state) {
+            is State.Ready -> R.string.ready
+            is State.Work -> R.string.state_work
+            is State.ShortBreak -> R.string.state_short_break
+            is State.LongBreak -> R.string.state_long_break
+        }
+    }
+
+    private fun onPomodoroCounterFinished(state: State) {
+        binding.buttonPlay.visibility = View.VISIBLE
+        binding.buttonPause.visibility = View.GONE
+        when (state) {
+            State.Work -> showNotification(
+                title = getString(R.string.notification_time_up_title),
+                text = getString(R.string.notification_time_up_text_work)
+            )
+
+            State.ShortBreak -> showNotification(
+                title = getString(R.string.notification_time_up_title),
+                text = getString(R.string.notification_time_up_text_short_break)
+            )
+
+            State.LongBreak -> showNotification(
+                title = getString(R.string.notification_time_up_title),
+                text = getString(R.string.notification_time_up_text_long_break)
+            )
+
+            State.Ready -> {}
+        }
     }
 
     private fun showNotification(title: String, text: String) {
@@ -62,5 +145,12 @@ class TimerActivity : AppCompatActivity() {
         }.build()
         val manager = NotificationManagerCompat.from(this)
         manager.notify(0, notification)
+    }
+
+    sealed class State {
+        object Ready : State()
+        object Work : State()
+        object ShortBreak : State()
+        object LongBreak : State()
     }
 }
