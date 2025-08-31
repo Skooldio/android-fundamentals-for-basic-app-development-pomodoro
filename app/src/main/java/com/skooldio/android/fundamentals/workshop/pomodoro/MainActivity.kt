d2f236427db051e4bee2faa8b8dae63f56888963
Akexorcist <akexorcist@gmail.com>
2025-09-01 03:48:40 +0700

    Workshop Part 6 - Use parcelable

--- app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
+++ app/src/main/java/com/skooldio/android/fundamentals/workshop/pomodoro/MainActivity.kt
@@ -130,11 +130,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonReady.setOnClickListener {
-            val intent = TimerActivity.newIntent(
-                context = this,
+            val config = Config(
                 workDuration = workDuration,
                 shortBreakDuration = shortBreakDuration,
                 longBreakDuration = longBreakDuration
+            )
+            val intent = TimerActivity.newIntent(
+                context = this,
+                config = config
             )
             startActivity(intent)
         }
